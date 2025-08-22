7723b853693c7914aa19e2828feee3497ad8cfd2
qwn3213 <qwn3213@qq.com>
2025-08-22 18:02:41 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -560,6 +560,7 @@ const googleModels = [
   "gemini-2.0-flash-thinking-exp-01-21",
   "gemini-2.0-pro-exp",
   "gemini-2.0-pro-exp-02-05",
+  "gemini-2.5-flash",
   "gemini-2.5-pro-preview-06-05",
   "gemini-2.5-flash-preview-04-17",
   "gemini-2.5-pro"
